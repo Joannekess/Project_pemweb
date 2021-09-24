0bc044c642d325f50054f934f464910a76619dbd
nehemiagueldi <90768578+nehemiagueldi@users.noreply.github.com>
2021-09-24 12:41:45 +0700

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,70 +1,98 @@
-<?php  
-	$host = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "pemwebuts";
+<?php
+// koneksi ke database
+$conn = mysqli_connect("localhost", "root", "", "pemwebw4");
 
-	//connect to database
-	$conn = mysqli_connect($host, $username, $password, $dbname);
+function query($query)
+{
+    global $conn;
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
 
-	function query($query) {
-		global $conn;
-		
-		$result = mysqli_query($conn, $query);
-		$rows = [];
-		while($row = mysqli_fetch_assoc($result)) {
-			$rows[] = $row;
-		}
-		return $rows;
-	}
 
-	function addNews($data) {
-		global $conn;
+function add($data)
+{
+    global $conn;
 
-		// $no = htmlspecialchars($data["no"]);
-		$judul = htmlspecialchars($data["judul"]);
-		$kategori = htmlspecialchars($data["kategori"]);
-		$penulis = htmlspecialchars($data["penulis"]);
-		$konten = htmlspecialchars($data["konten"]);
-		$tanggal = htmlspecialchars($data["tanggal"]);
+    $studentid = htmlspecialchars($data["StudentID"]);
+    $firstname = htmlspecialchars($data["FirstName"]);
+    $lastname = htmlspecialchars($data["LastName"]);
 
-		//query insert data
-		$query = "INSERT INTO news VALUES('', '$judul', '$kategori', '$penulis', '$konten', '$tanggal')";
-		mysqli_query($conn, $query);
+    // query insert data
+    $query = "INSERT INTO datam
+                VALUES
+              ('', '$studentid', '$firstname', '$lastname')";
 
-		return mysqli_affected_rows($conn); 
-	}
+    mysqli_query($conn, $query);
 
-	function delete($id) {
-		global $conn;
-		mysqli_query($conn, "DELETE FROM news WHERE no = $id");
+    return mysqli_affected_rows($conn);
+}
 
-		return mysqli_affected_rows($conn);
-	}
 
-	function update($data) {
-		global $conn;
+function delete($id)
+{
+    global $conn;
+    mysqli_query($conn, "DELETE FROM datam WHERE StudentID = $id");
 
-		$id = $data["id"];
-		$no = htmlspecialchars($data["no"]);
-		$judul = htmlspecialchars($data["judul"]);
-		$kategori = htmlspecialchars($data["kategori"]);
-		$penulis = htmlspecialchars($data["penulis"]);
-		$konten = htmlspecialchars($data["konten"]);
-		$tanggal = htmlspecialchars($data["tanggal"]);
+    return mysqli_affected_rows($conn);
+}
 
-		//query update data
-		$query = "UPDATE news 
-				  SET no = '$no',
-				  	  judul = '$judul', 
-				  	  kategori = '$kategori',
-				  	  penulis = '$penulis',
-				  	  konten = '$konten',
-				  	  tanggal = '$tanggal'
-				  WHERE no = $id;
-				  ";
-		mysqli_query($conn, $query);
 
-		return mysqli_affected_rows($conn); 
-	}
-?>
+function update($data)
+{
+    global $conn;
+
+    $id = htmlspecialchars($data["ID"]);
+    // $studentid = htmlspecialchars($data["StudentID"]);
+    $firstname = htmlspecialchars($data["FirstName"]);
+    $lastname = htmlspecialchars($data["LastName"]);
+
+    // query insert data
+    $query = "UPDATE datam SET 
+                FirstName = '$firstname',
+                LastName = '$lastname'
+              WHERE ID = $id
+              ";
+
+    mysqli_query($conn, $query);
+
+    return mysqli_affected_rows($conn);
+}
+
+function registrasi($data)
+{
+    global $conn;
+
+    $username = strtolower(stripslashes($data["username"]));
+    $password = mysqli_real_escape_string($conn, $data["password"]);
+    $password2 = mysqli_real_escape_string($conn, $data["password2"]);
+
+    // cek username sudah ada atau belum
+    $result = mysqli_query($conn, "SELECT email FROM user WHERE email = '$username'");
+    if (mysqli_fetch_assoc($result)) {
+        echo "<script>
+            alert('Username sudah terdaftar');
+          </script>";
+        return false;
+    }
+
+    // cek konfirmasi password
+    if ($password !== $password2) {
+        echo "<script>
+            alert('Konfirmasi password tidak sesuai!');
+          </script>";
+        return false;
+    }
+
+    // enkripsi password
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+
+    // tambahkan user baru ke database
+    mysqli_query($conn, "INSERT INTO user VALUES ('', '$username', '$password')");
+    return mysqli_affected_rows($conn);
+}
